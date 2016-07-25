edc03943f9ccf988d169750367331992e16a2d52
Raymond Sze <raymond.sze@iheha.com>
2016-07-25 17:35:37 +0800

    fix eslint

--- src/producers/example.js
+++ src/producers/example.js
@@ -6,13 +6,17 @@ export default (queue) => {
   .backoff({ type: 'exponential' })
   .attempts(5)
   .save();
-  job.on('complete', (result) => {
-    console.log('Job completed with data ', result);
-  }).on('failed attempt', (errorMessage, doneAttempts) => {
-    console.log('Job failed');
-  }).on('failed', (errorMessage) => {
-    console.log('Job failed');
-  }).on('progress', (progress, data) => {
-    console.log('\r  job #' + job.id + ' ' + progress + '% complete with data ', data );
-  });
+  job
+    .on('complete', (result) => {
+      console.log('Job completed with data ', result);
+    })
+    .on('failed attempt', (/* errorMessage, doneAttempts */) => {
+      console.log('Job failed');
+    })
+    .on('failed', (/* errorMessage */) => {
+      console.log('Job failed');
+    })
+    .on('progress', (progress, data) => {
+      console.log(`\r  job #${job.id}  progress % complete with data ${data}`);
+    });
 };
